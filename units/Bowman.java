659c26d72797b338306a4d849c07a56099c040d9
Yaroslav <Valso96@mail.ru>
2023-04-06 18:05:38 +0600

    added files

---
+++ units/Bowman.java
@@ -0,0 +1,13 @@
+package seminar1.units;
+
+public class Bowman extends Shooter {
+
+    public Bowman(String name, boolean firstTeam) {
+        super("Лучник",50, name, firstTeam, 10, new int[]{8, 16},  10, 60);
+    }
+
+    @Override
+    public String getInfo() {
+        return className + " " + name;
+    }
+}
---
+++ units/GameInterface.java
@@ -0,0 +1,12 @@
+package seminar1.units;
+
+import seminar1.teams.Team;
+
+import java.util.ArrayList;
+
+public interface GameInterface {
+
+    String getInfo();
+
+    void step();
+}
